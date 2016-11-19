27ba895173da7e7173a597bc001232bf47f0a54f
zzzuka <zuka-monstr.yes@yandex.ru>
2016-11-19 03:21:00 +0300

    Added classes 'Hero', 'RedDragon', 'BlackDragon'

--- enemies.py
+++ enemies.py
@@ -39,8 +39,31 @@ class GreenDragon(Dragon):
         self.set_answer(x + y)
         return self.__quest
 
-#FIXME здесь также должны быть описаны классы RedDragon и BlackDragon
-# красный дракон учит вычитанию, а чёрный -- умножению.
+class RedDragon(Dragon):
+    def __init__(self):
+        self._health = 200
+        self._attack = 10
+        self._color = 'красный'
+
+    def question(self):
+        x = randint(1, 100)
+        y = randint(1, 100)
+        self.__quest = str(x) + '-' + str(y)
+        self.set_answer(x - y)
+        return self.__quest
+
+class BlackDragon(Dragon):
+    def __init__(self):
+        self._health = 200
+        self._attack = 10
+        self._color = 'чёрный'
+
+    def question(self):
+        x = randint(1, 100)
+        y = randint(1, 100)
+        self.__quest = str(x) + '*' + str(y)
+        self.set_answer(x * y)
+        return self.__quest
 
 
 enemy_types = [GreenDragon, RedDragon, BlackDragon]
--- hero.py
+++ hero.py
@@ -2,9 +2,9 @@
 # license: GPLv3
 from gameunit import *
 
-#FIXME:
-"""В этом файле должен быть описан класс героя, унаследованный от Attacker
-Герой должен иметь 100 поинтов здоровья, атаку 50, опыт 0, имя, задаваемое в конструкторе
-Метод attack должен получать атрибут target и уменьшать его здоровье на величину атаки.
-
-"""
+class Hero(Attacker):
+    def __init__(self, name):
+        self._name = name
+        self._health = 100
+        self._attack = 50
+        self._experience = 0
